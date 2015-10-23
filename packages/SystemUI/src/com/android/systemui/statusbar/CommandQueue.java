152d2654d71043a89e9ba3ff327fa7b88276dc88
Lars Greiss <kufikugel@googlemail.com>
2015-10-23 04:37:46 +0000

    Add back all keyboard IME features back [1/2]
    this commit adds:
    
    - ime switcher notification - thanks cm
    - slims fullscreen disable on landscape
    - slims force autorotation on IME
    - volume button control for navigate in the message - thanks cm
    - slims force to show always enter key
    
    some fixes to the 4.2/4.3 implementations + added full multiuser
    support for all this features
    
    cheers
    
    Change-Id: I71e93c39620cc171e658b2d7e26491bac97c999d
    Signed-off-by: AudioGod <audiogod@sonic-developers.com>
    
    Conflicts:
    	core/java/com/android/internal/statusbar/IStatusBar.aidl
    	packages/SystemUI/src/com/android/systemui/statusbar/CommandQueue.java
    
    Conflicts:
    	core/java/android/inputmethodservice/InputMethodService.java
    	core/res/res/values/sshd_config.xml
    	core/res/res/values/sshd_symbols.xml
    	packages/SystemUI/src/com/android/systemui/statusbar/CommandQueue.java

--- packages/SystemUI/src/com/android/systemui/statusbar/CommandQueue.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/CommandQueue.java
@@ -65,6 +65,7 @@ public class CommandQueue extends IStatusBar.Stub {
     private static final int MSG_ASSIST_DISCLOSURE          = 22 << MSG_SHIFT;
     private static final int MSG_START_ASSIST               = 23 << MSG_SHIFT;
     private static final int MSG_CAMERA_LAUNCH_GESTURE      = 24 << MSG_SHIFT;
+    private static final int MSG_SET_AUTOROTATE_STATUS      = 25 << MSG_SHIFT;
     private static final int MSG_TOGGLE_LAST_APP            = 26 << MSG_SHIFT;
     private static final int MSG_TOGGLE_KILL_APP            = 27 << MSG_SHIFT;
     private static final int MSG_TOGGLE_SCREENSHOT          = 28 << MSG_SHIFT;
@@ -115,6 +116,7 @@ public class CommandQueue extends IStatusBar.Stub {
         public void showAssistDisclosure();
         public void startAssist(Bundle args);
         public void onCameraLaunchGestureDetected(int source);
+        public void setAutoRotate(boolean enabled);
         public void toggleLastApp();
         public void toggleKillApp();
         public void toggleScreenshot();
@@ -314,6 +316,14 @@ public class CommandQueue extends IStatusBar.Stub {
             mHandler.obtainMessage(MSG_CAMERA_LAUNCH_GESTURE, source, 0).sendToTarget();
         }
     }
+	
+    public void setAutoRotate(boolean enabled) {
+        synchronized (mList) {
+            mHandler.removeMessages(MSG_SET_AUTOROTATE_STATUS);
+            mHandler.obtainMessage(MSG_SET_AUTOROTATE_STATUS,
+                enabled ? 1 : 0, 0, null).sendToTarget();
+        }
+    }
 
     public void pause() {
         mPaused = true;
@@ -448,6 +458,8 @@ public class CommandQueue extends IStatusBar.Stub {
                     break;
                 case MSG_CAMERA_LAUNCH_GESTURE:
                     mCallbacks.onCameraLaunchGestureDetected(msg.arg1);
+                case MSG_SET_AUTOROTATE_STATUS:
+                    mCallbacks.setAutoRotate(msg.arg1 != 0);
                     break;
                 case MSG_TOGGLE_LAST_APP:
                     mCallbacks.toggleLastApp();
