d75224453f45207102033fefc9f97242ae0b8687
AudioGod <audiogod@sonic-developers.com>
2016-01-10 15:14:11 +0530

    Revert "InputMethodService: Disable fullscreen keyboard."
    This reverts commit d68198ff7c3e7e084ac78d15af338681e3a02f28.
    
    Signed-off-by: AudioGod <audiogod@sonic-developers.com>
    
    Conflicts:
    	core/java/android/inputmethodservice/InputMethodService.java

--- core/java/android/inputmethodservice/InputMethodService.java
+++ core/java/android/inputmethodservice/InputMethodService.java
@@ -1,6 +1,5 @@
 /*
  * Copyright (C) 2007-2008 The Android Open Source Project
- * This code has been modified. Portions copyright (C) 2013, ParanoidAndroid Project.
  * 
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
@@ -954,7 +953,7 @@ public class InputMethodService extends AbstractInputMethodService {
         } else {
             isFullscreen = mShowInputRequested && onEvaluateFullscreenMode();
         }
-        boolean isFullscreen = false;
+        boolean isFullscreen = mShowInputRequested && onEvaluateFullscreenMode();
         boolean changed = mLastShowInputRequested != mShowInputRequested;
         if (mIsFullscreen != isFullscreen || !mFullscreenApplied) {
             changed = true;
